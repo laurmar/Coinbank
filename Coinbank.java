42efb8e72e883c487ed75f814fd7c0fa82c1f524
laurmar <36283876+laurmar@users.noreply.github.com>
2019-08-11 09:07:19 -0500

    Updated coinbanck

--- Coinbank.java
+++ Coinbank.java
@@ -1,16 +1,18 @@
+package proj1;  // Don't change the package name.  Gradescope expects this.
 
-import java.util.Arrays;
 /**
+ *
+ * This class represents a mid 20th century coinbank. Users will be able to create coinbanks along with input
+ * and remove coins essentially mimicking a functioning coinbank.
  * @author Laura Marlin
- * Data Structures
- * @version 9/16/17
- * This class represents a coin bank for pennies, nickels, dimes and quarters. 
- * Users can insert and remove coins, seeing the total amount within the bank and the amount they remove.
+ * @version September 16, 2018
+ *
+ *
+ * I affirm that I have carried out the attached academic endeavors with full academic honesty,
+ * in accordance with the Union College Honor Code and the course syllabus.
+ *
  */
-
-
 public class Coinbank {
-
 
 	// Denominations
 	public static final int PENNY_VALUE = 1;
@@ -19,85 +21,136 @@ public class Coinbank {
 	public static final int QUARTER_VALUE = 25;
 
 
-	private final int PENNY= 0;
+	// give meaningful names to holder array indices
+	private final int PENNY = 0;
 	private final int NICKEL = 1;
-	private final int DIME= 2;
+	private final int DIME = 2;
 	private final int QUARTER = 3;
 
+
+	// how many types of coins does the bank hold?
 	private final int COINTYPES = 4;
 
-	
+
 	private int[] holder;
 
 
-	public Coinbank() {
-		holder= new int[] {0,0,0,0};
+	//CONSTRUCTOR SECTION
+	//------------------------------------------------------------------------------------------------------------------
 
+
+	/**
+	 * Default constructor
+	 */
+	public Coinbank() {
+		holder = new int[COINTYPES];
+	}
+
+
+
+	// GETTERS AND SETTERS SECTION
+	//------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Gets the holder
+	 * @return the instance variable holder
+	 */
+	private int[] getHolder(){
+		return holder;
+	}
+
+	/**
+	 * Set the holder to a new value
+	 * @param newHolder a new holder to replace the current holder with
+	 */
+	private void setHolder(int[] newHolder){
+		holder= newHolder;
 
 	}
+
+
+
+	/**
+	 * This function uses coin values to discover the quantity of coins that are contained of
+	 * a certain coin type.
+	 *
+	 *NOTE: This function relies on the holder being in the form of an array of length 4.Also -1 is used
+	 * to indicate when an invalid denomination has been inputted
+	 * @param coinType denomination of coin to get. Valid denominations are 1,5,10,25
+	 * @return number of coins that bank is holding of that type, or -1 if denomination not valid
+	 */
+	public int get(int coinType) {
+		int location= getCoinbankSpot(coinType);
+		if(location == -1) {
+			return -1;
+
+		}
+		return holder[location];
+	}
+
+
+	/**
+	 * This function navigates to the correct location given a coin value
+	 *
+	 * @param coinType: this is denominations of coins that can be inputted. Valid denominations are 1,5,10,25
+	 * @return the spot that the coins of that type are contained within or -1 if the inputted type is invalid
+	 */
+	private int getCoinbankSpot(int coinType) {
+		if(isBankable(coinType)) {
+			if (coinType == PENNY_VALUE) {
+				return PENNY;
+			} else if (coinType == NICKEL_VALUE) {
+				return NICKEL;
+			} else if (coinType == DIME_VALUE) {
+				return DIME;
+			} else {
+				return QUARTER;
+			}
+		}
+		else{
+			return -1;
+		}
+
+	}
+
 
 
 
 
 	/**
-	 * getter 
-	 * @param coinType denomination of coin to get. Valid denominations are 1,5,10,25
-	 * @return number of coins that bank is holding of that type, or -1 if denomination not valid
-	 */
-	public int get(int coinType){
-		if (coinType==PENNY_VALUE) {
-			return holder[PENNY];
-		}
-		else if (coinType== NICKEL_VALUE) {
-			return holder[NICKEL];
-		}
-		else if (coinType== DIME_VALUE) {
-			return holder[DIME];
-		}
-		else if (coinType== QUARTER_VALUE) {
-			return holder[QUARTER];
-		}
-		else {
-			return -1;
-		}
-	}
-
-
-
-	/** 
-	 * setter
+	 * This function sets the quantity of coins of a selected cointype to a new value of the users
+	 * choosing.
+	 *
+	 * NOTE: This function relies on the holder being in the form of an array of length 4. Also -1 is used
+	 * to indicate when an invalid denomination has been inputted
 	 * @param coinType denomination of coin to set
 	 * @param numCoins number of coins
 	 */
 	private void set(int coinType, int numCoins) {
-		if (coinType==PENNY_VALUE) {
-			holder[PENNY]= numCoins;
+		if (numCoins >= 0) {
+			int location= getCoinbankSpot(coinType);
+			if(location!=-1) {
+				holder[location] = numCoins;
+			}
 		}
-		else if (coinType== NICKEL_VALUE) {
-			holder[NICKEL]= numCoins;
-		}
-		else if (coinType== DIME_VALUE) {
-			holder[DIME]= numCoins;
-		}
-		else if (coinType== QUARTER_VALUE) {
-			holder[QUARTER]= numCoins;
-		}
-		else {
-			return;
-		}
-
 	}
+
+	// GENERAL METHODS
+	//------------------------------------------------------------------------------------------------------------------
 
 
 	/**
 	 * Return true if given coin can be held by this bank.  Else false.
+	 *
 	 * @param coin penny, nickel, dime, or quarter is bankable.  All others are not.
 	 * @return true if bank can hold this coin, else false
 	 */
-	private boolean isBankable(int coin){
+	private boolean isBankable(int coin) {
 		switch (coin) {
-			case PENNY_VALUE: case NICKEL_VALUE:
-			case DIME_VALUE: case QUARTER_VALUE:
+			case PENNY_VALUE:
+			case NICKEL_VALUE:
+			case DIME_VALUE:
+			case QUARTER_VALUE:
 				return true;
 			default:
 				return false;
@@ -113,83 +166,57 @@ public class Coinbank {
 	 * @param coinType either 1, 5, 10, or 25 to be valid
 	 * @return true if deposit successful, else false
 	 */
-	// how reusable is this?
-	public boolean insert(int coinType){
+	public boolean insert(int coinType) {
 		if (!isBankable(coinType)) {
 			return false;
-		}
-		else {
-			set(coinType, get(coinType)+1);
+		} else {
+			set(coinType, get(coinType) + 1);
 			return true;
 		}
 	}
 
-	
-	/**
-	 * Checks to see whether a valid number of coins is being requested
-	 * @param requestedCoins int representing the number of coins requested
-	 * @return True if it is a positive number, else false
-	 */
-	
-	private boolean isValidRequestedCoin(int requestedCoins) {
-		if (requestedCoins>= 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
 
-	/**
-	 * Determines the number of coins removed from the bank
-	 * @param coinType 5,10,1,25 for each of the coin values
-	 * @param requestedCoins the number of coins requested in an int
-	 * @return the number of coins that is removed from the bank
-	 */
-	private int getNumRemoved(int coinType, int requestedCoins) {
-		int numRemoved=0;
-		if (get(coinType)>= requestedCoins) { 
-			numRemoved= requestedCoins;
-			}
-		else if (get(coinType)< requestedCoins) { 
-			numRemoved= get(coinType);
-			}
-		return numRemoved;		
-	}
-	
-	
+
 	/**
 	 * returns the requested number of the requested coin type, if possible.
 	 * Does nothing if the coin type is invalid.  If bank holds
 	 * fewer coins than is requested, then all of the coins of that
 	 * type will be returned.
-	 * @param coinType either 1, 5, 10, or 25 to be valid
+	 *
+	 * Note: coinsWeHave returns -1 when the coinType is invalid
+	 * @param coinType       either 1, 5, 10, or 25 to be valid
 	 * @param requestedCoins number of coins to be removed
 	 * @return number of coins that are actually removed
 	 */
 	public int remove(int coinType, int requestedCoins) {
-		if (isBankable(coinType) & isValidRequestedCoin(requestedCoins)) {
-			int numRemoved= getNumRemoved(coinType, requestedCoins);
-			set(coinType, numLeft(requestedCoins, get(coinType)));
-			return numRemoved; 
-		}		
-		else {
-			return 0; 
-			}
-
+		int removedCoins = 0;
+		int coinsWeHave;
+		int coinsLeft;
+		coinsWeHave = get(coinType);
+		if (coinsWeHave>= 0 && requestedCoins >= 0) {
+			coinsLeft = numLeft(requestedCoins, coinsWeHave);
+			set(coinType, coinsLeft);
+			removedCoins = coinsWeHave - coinsLeft;
+		}
+		return removedCoins;
 	}
+
+
 
 
 	/**
 	 * returns number of coins remaining after removing the
 	 * requested amount.  Returns zero if requested amount > what we have
+	 *
 	 * @param numWant number of coins to be removed
 	 * @param numHave number of coins you have
 	 * @return number of coins left after removal
 	 */
-	private int numLeft(int numWant, int numHave){
-		return Math.max(0, numHave-numWant);
+	private int numLeft(int numWant, int numHave) {
+
+		return Math.max(0, numHave - numWant);
 	}
+
 
 	/**
 	 * Returns bank as a printable string
@@ -201,13 +228,12 @@ public class Coinbank {
 				get(QUARTER_VALUE) * QUARTER_VALUE) / 100.0;
 
 		String toReturn = "The bank currently holds $" + total + " consisting of \n";
-		toReturn+=get(PENNY_VALUE) + " pennies\n";
-		toReturn+=get(NICKEL_VALUE) + " nickels\n";
-		toReturn+=get(DIME_VALUE) + " dimes\n";
-		toReturn+=get(QUARTER_VALUE) + " quarters\n";
+		toReturn += get(PENNY_VALUE) + " pennies\n";
+		toReturn += get(NICKEL_VALUE) + " nickels\n";
+		toReturn += get(DIME_VALUE) + " dimes\n";
+		toReturn += get(QUARTER_VALUE) + " quarters\n";
 		return toReturn;
 	}
-	
-	
+
 
 }
